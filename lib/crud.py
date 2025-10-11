cc64671e8335201389df4e136859ba8ef81d0d7f
23017998 <23017998@uhi.ac.uk>
2025-10-11 07:55:33 +0100

    database rework

--- lib/crud.py
+++ lib/crud.py
@@ -1,7 +1,8 @@
 import sqlite3
 import os
 from lib.databases.CreationQueries import Creator
-
+import re
+import hashlib
 
 ###
 ### This class is responsible for all operations done on databases.
@@ -14,21 +15,20 @@ from lib.databases.CreationQueries import Creator
 class CRUD(Creator):    
     def __init__(self):
         self.DATABASE_DIR = "databases"
-        self.PROJECTS_DB = "projects.sqlite3"
-        self.FEEDBACK_DB = "feedbacks.sqlite3"
+        self.DB_FILENAME = "db.sqlite3"
         super().__init__()
         self.initialize_databases()
         
     def initialize_databases(self):
-        self.open_connection(self.PROJECTS_DB)
-        self.execute_query(self.create_projects_query)
-        self.close_connection()
-        self.open_connection(self.FEEDBACK_DB)
-        self.execute_query(self.create_feedbacks_query)
+        self.open_connection()
+        self.execute_query(self.create_projects_table)
+        self.execute_query(self.create_feedbacks_table)
+        self.execute_query(self.create_sessions_table)
+        self.execute_query(self.create_users_table)
         self.close_connection()
         
-    def open_connection(self, db):
-        path = os.path.join(os.getcwd(), "lib", self.DATABASE_DIR, db)
+    def open_connection(self):
+        path = os.path.join(os.getcwd(), "lib", self.DATABASE_DIR, self.DB_FILENAME)
         self.connection = sqlite3.connect(path)
         self.connection.execute('PRAGMA foreign_keys = ON')
         self.cursor = self.connection.cursor()
@@ -40,8 +40,8 @@ class CRUD(Creator):
     def execute_query(self, query):
         self.cursor.execute(query)
         
-    def create(self, database, table: str, values=(), columns=()):
-        self.open_connection(database)
+    def create(self, table: str, values=(), columns=()):
+        self.open_connection()
         if columns == ():
             secureQuery = f"INSERT INTO {table} VALUES ({self.get_values_placeholder(values)})"
         else:
@@ -51,20 +51,31 @@ class CRUD(Creator):
         self.execute_query_securely(secureQuery, values)
         self.close_connection()
         
-    def read(self, database, table:str, selection="*", whereColumn=None, whereValue=None):
-        self.open_connection(database)
+    
+    def read(self, table: str, selection="*", whereColumn=None, whereValue=None):
+        def is_safe_identifier(name):
+            return bool(re.match(r'^[A-Za-z_][A-Za-z0-9_]*$', name))
+
+        if not is_safe_identifier(table):
+            raise ValueError(f"Invalid table name: {table}")
+        if selection != "*" and not all(is_safe_identifier(s.strip()) for s in selection.split(",")):
+            raise ValueError(f"Invalid selection columns: {selection}")
+        if whereColumn is not None and not is_safe_identifier(whereColumn):
+            raise ValueError(f"Invalid WHERE column: {whereColumn}")
+
+        self.open_connection()
         if whereColumn is None:
-            self.cursor.execute(f"SELECT {selection} FROM {table}")
-            result = self.cursor.fetchall()
+            query = f"SELECT {selection} FROM {table}"
+            self.cursor.execute(query)
         else:
             query = f"SELECT {selection} FROM {table} WHERE {whereColumn} = ?"
             self.cursor.execute(query, (whereValue,))
-            result = self.cursor.fetchall()
+        result = self.cursor.fetchall()
         self.close_connection()
         return result
     
-    def update(self, database, table: str, columns, whereColumn: str, whereValue, values):
-        self.open_connection(database)
+    def update(self, table: str, columns, whereColumn: str, whereValue, values):
+        self.open_connection()
         set_clause = ", ".join([f"{col} = ?" for col in columns])
         query = f"UPDATE {table} SET {set_clause} WHERE {whereColumn} = ?"
         
@@ -77,8 +88,8 @@ class CRUD(Creator):
         self.execute_query_securely(query, parameters)
         self.close_connection()
 
-    def delete(self, database, table: str, whereColumn: str, whereValue):
-        self.open_connection(database)
+    def delete(self, table: str, whereColumn: str, whereValue):
+        self.open_connection()
         query = f"DELETE FROM {table} WHERE {whereColumn} = ?"
         self.execute_query_securely(query, (whereValue,))
         self.close_connection()
@@ -98,7 +109,7 @@ class CRUD(Creator):
         
     def get_all_projects(self):
         all_projects = {}
-        for record in self.read(self.PROJECTS_DB, "Projects"):    
+        for record in self.read("Projects"):    
             all_projects[record[0]] = {}
             all_projects[record[0]]["project_ID"] = record[0]
             all_projects[record[0]]['project_name'] = record[1]
@@ -107,7 +118,7 @@ class CRUD(Creator):
     
     def get_all_feedbacks(self):
         all_feedbacks = {}
-        for record in self.read(self.FEEDBACK_DB, "Feedbacks"):
+        for record in self.read("Feedbacks"):
             all_feedbacks[record[0]] = {}
             all_feedbacks[record[0]]["feedback_ID"] = record[0]
             all_feedbacks[record[0]]["author"] = record[1]
